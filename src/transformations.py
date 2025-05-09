cd7120530144c9a8fe3bb72ebca446f9eaf8b753
Shiny5228 <187951996+Shiny5228@users.noreply.github.com>
2025-05-09 18:42:45 +0200

    Moved charts to dash for better filter performance (#1)
    Moved charts to dash for better performance. Dashboard now shows PNL Graph and indicators for PCR % und Win rate %.
    Plotly charts filtering had too much limitations.
    Removed some unsed files and functions and reworked the transform function.
    The functions now used fifoPnlRealized if the trade is settled and mtmpnl if the trade is closed but not yet settled. The opening date/time is now extracted to the closed position to group combo trades. This was necessary for the correct calcutions for win rate.

--- src/transformations.py
+++ src/transformations.py
@@ -4,43 +4,18 @@ import numpy as np
 import pandas as pd
 
 
-def filter_and_group_data(
-    df,
-    assets_to_include=None,
-    symbols_to_include=None,
-    dates_to_exclude=None,
-):
+def transform(df):
+
     """
     Filters and groups the given DataFrame based on specified criteria.
 
     Parameters:
         df (pandas.DataFrame): The DataFrame to filter and group.
-        assets_to_include (list, optional): List of asset categories to include, e.g., ["STK", "OPT", "FUT", "FOP"]. Defaults to None.
-        symbols_to_include (list, optional): List of underlying symbols to include. Defaults to None.
-        dates_to_exclude (list, optional): List of trade dates to exclude. Defaults to None.
 
     Returns:
         pandas.DataFrame: A grouped DataFrame with calculated PnLRealized.
     """
 
-    # Set default values if parameters are None
-    if assets_to_include is None:
-        assets_to_include = df["assetCategory"].unique()
-    if symbols_to_include is None:
-        symbols_to_include = df["underlyingSymbol"].unique()
-    if dates_to_exclude is None:
-        dates_to_exclude = []
-
-    # tradeDate is in datetime format and dates_to_exclude is converted to datetime
-    dates_to_exclude = pd.to_datetime(
-        dates_to_exclude, format="%Y%m%d", errors="coerce"
-    )
-
-    filtered_df = df[
-        (df["assetCategory"].isin(assets_to_include))
-        & (df["underlyingSymbol"].isin(symbols_to_include))
-        & (~df["tradeDate"].isin(dates_to_exclude))
-    ]
 
     # Ensure consistent datetime format for comparison
     current_date = pd.Timestamp(datetime.now().date())
@@ -53,145 +28,52 @@ def filter_and_group_data(
     # Because of weekends and holidays the settlement date is not always t+1 and can be t+2 or t+3.
 
     condition = (
-        (filtered_df["tradeDate"] == filtered_df["expiry"])
-        & (filtered_df["fifoPnlRealized"] == 0)
-        & (filtered_df["settleDateTarget"].between(current_date, future_date))
+        (df["tradeDate"] == df["expiry"])
+        & (df["fifoPnlRealized"] == 0)
+        & (df["settleDateTarget"].between(current_date, future_date))
     )
 
-    # mtmPnL does noet include the commission, so we add it to the mtmPnl to get the correct PnLRealized.
-    mtmPnl_commission = filtered_df["mtmPnl"] + filtered_df["ibCommission"]
+    # mtmPnL does not include the commission, so we add it to the mtmPnl to get the correct PnLRealized.
+    mtmPnl_commission = df["mtmPnl"] + df["ibCommission"]
 
     # Set PnLRealized to mtmPnl + ibCommission if the condition is met, otherwise use fifoPnlRealized
-    filtered_df["PnLRealized"] = np.where(
-        condition, mtmPnl_commission, filtered_df["fifoPnlRealized"]
-    )
+    df["PnLRealized"] = np.where(condition, mtmPnl_commission, df["fifoPnlRealized"])
 
-    # Group by assetCategory, underlyingSymbol, tradeDate, and settleDateTarget and sum the PnLRealized
-    grouped_df = (
-        filtered_df.groupby(["tradeDate", "settleDateTarget"])["PnLRealized"]
-        .sum()
-        .reset_index()
-    )
+    # Initialisiere die neue Spalte mit leeren Werten
 
-    return filtered_df, grouped_df
+    df["opendateTime"] = pd.NaT
+    df["opendateTime"] = df["opendateTime"].astype("datetime64[ns]")
 
+    # Gruppiere nach 'description'
+    grouped = df.groupby("description")
+    for name, group in grouped:
+        if len(group) == 1 and group["openCloseIndicator"].iloc[0] == "O":
+            df.loc[df["description"] == name, "opendateTime"] = group["dateTime"].iloc[
+                0
+            ]
+        else:
+            if "O" in group["openCloseIndicator"].values:
+                open_datetime = group[group["openCloseIndicator"] == "O"][
+                    "dateTime"
+                ].iloc[0]
+                df.loc[
+                    (df["description"] == name) & (df["openCloseIndicator"] == "C"),
+                    "opendateTime",
+                ] = open_datetime
+            else:
+                open_datetime = group["dateTime"].iloc[0]
+                df.loc[
+                    (df["description"] == name) & (df["openCloseIndicator"] == "C"),
+                    "opendateTime",
+                ] = open_datetime
 
-def add_cum_sum_rows(df):
-    """
-    Adds cumulative sum columns to the DataFrame for different time periods.
-
-    Parameters:
-        df (pandas.DataFrame): The DataFrame to which cumulative sum columns will be added.
-
-    Returns:
-        pandas.DataFrame: The modified DataFrame with additional cumulative sum columns.
-    """
-
-    # cumulative sum
-    df["TotalProfit"] = df["PnLRealized"].cumsum()
-
-    # cumulative sum per calendarweek
-    df["ProfitWeek"] = df.groupby(df["tradeDate"].dt.isocalendar().week)[
-        "PnLRealized"
-    ].cumsum()
-
-    # cumulative sum per month
-    df["ProfitMonth"] = df.groupby(df["tradeDate"].dt.to_period("M"))[
-        "PnLRealized"
-    ].cumsum()
-
-    # cumulative sum per quarter
-    df["ProfitQuarter"] = df.groupby(df["tradeDate"].dt.to_period("Q"))[
-        "PnLRealized"
-    ].cumsum()
-
-    df["week"] = df["tradeDate"].dt.isocalendar().week
-    df["month"] = df["tradeDate"].dt.month_name()
-    df["quarter"] = df["tradeDate"].dt.quarter
-    df["year"] = df["tradeDate"].dt.year
+    # Filter for closed trades except for the ones that are not settled yet, like 0DTE options
+    df = df[
+        (df["openCloseIndicator"] == "C")
+        | (
+            (df["settleDateTarget"].between(current_date, future_date))
+            & (df["PnLRealized"] != 0)
+        )
+    ]
 
     return df
-
-
-# Prepare data for visualization
-def get_filtered_data(df, period, value=None):
-    """
-    Filters a DataFrame based on a specified time period and value.
-
-    Parameters:
-        df (pandas.DataFrame): The DataFrame to filter. It is expected to have columns
-                               such as 'week', 'month', 'quarter', and 'year'.
-        period (str): The time period to filter by. Accepted values are "week", "month",
-                      "quarter", or "year".
-        value (optional): The value to filter the specified period by. If None, the
-                          function returns the original DataFrame.
-
-    Returns:
-        pandas.DataFrame: A filtered DataFrame containing rows that match the specified
-                          period and value. If the period is not recognized, the original
-                          DataFrame is returned.
-    """
-    if period == "week":
-        return df[df["week"] == value]
-    elif period == "month":
-        return df[df["month"] == value]
-    elif period == "quarter":
-        return df[df["quarter"] == value]
-    elif period == "year":
-        return df[df["year"] == value]
-    else:
-        return df
-
-
-def get_filtered_pcr(df, period, value=None):
-    """
-    Filters a DataFrame based on a specified time period and calculates the Premium Capture Rate (PCR).
-
-    Parameters:
-        df (pandas.DataFrame): The DataFrame to filter. It must contain the columns 'tradeDate', 'cost', and 'PnLRealized'.
-        period (str): The time period to filter by. Accepted values are "week", "month", "quarter", or "year".
-        value (optional): The value to filter the specified period by. If None, the function processes the entire DataFrame.
-
-    Returns:
-        None: Prints the premium received, premium captured, and premium capture rate.
-    """
-    # Ensure the DataFrame has the necessary columns
-    required_columns = {"tradeDate", "cost", "PnLRealized"}
-    if not required_columns.issubset(df.columns):
-        raise ValueError(
-            f"The DataFrame must contain the following columns: {required_columns}"
-        )
-
-    # Zeitspalten erzeugen
-    df["week"] = df["tradeDate"].dt.isocalendar().week
-    df["month"] = df["tradeDate"].dt.month_name()
-    df["quarter"] = df["tradeDate"].dt.quarter
-    df["year"] = df["tradeDate"].dt.year
-
-    # Nach Zeitraum filtern
-    if period == "week":
-        filtered_df = df[df["week"] == value]
-    elif period == "month":
-        filtered_df = df[df["month"] == value]
-    elif period == "quarter":
-        filtered_df = df[df["quarter"] == value]
-    elif period == "year":
-        filtered_df = df[df["year"] == value]
-    else:
-        filtered_df = df
-
-    # Nur verkaufte Optionen filtern (cost < 0)
-    df_pcr = filtered_df[filtered_df["cost"] < 0]
-
-    # Summen berechnen
-    sum_cost = abs(df_pcr["cost"].sum()).round(2)
-    sum_realized = df_pcr["PnLRealized"].sum().round(2)
-
-    # Premium Capture Rate berechnen
-    premium_capture_rate = (
-        ((sum_realized / sum_cost) * 100).round(2) if sum_cost != 0 else 0.0
-    )
-
-    print(f"Premium received: {sum_cost}")
-    print(f"Premium captured: {sum_realized}")
-    print(f"Premium capture rate: {premium_capture_rate}")
